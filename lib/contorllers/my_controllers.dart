87a1465ef6d91752046900f82c9a795ffe6ed12a
mlatifr <mlatifraldi@gmail.com>
2022-06-05 05:31:55 +0700

    everAll

--- lib/contorllers/my_controllers.dart
+++ lib/contorllers/my_controllers.dart
@@ -2,12 +2,20 @@ import 'package:get/get.dart';
 
 class MyController extends GetxController {
   var count = 0.obs;
+  var data = 0.obs;
   void change() => count++;
   void reset() => count.value = 0;
   @override
   void onInit() {
     print('onInit');
-    ever(count, (_) => print('DIJALANKAN'));//untuk menjalankan method setiap ada perubahan pada variable count di line 4
+    ever(
+        count,
+        (_) => print(
+            'ever DIJALANKAN')); //untuk menjalankan method setiap ada perubahan pada variable count di line 4
+    everAll(
+        [count, data],
+        (_) => print(
+            'everAll DIJALANKAN')); //mirip ever tapi list. jadi setiap ada perubahan pada list variable tertentu, maka akan menjalankan suatu method
     super.onInit();
   }
 }
